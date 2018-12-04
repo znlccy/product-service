4a1a8ac0478e07a4948c50c2e3b4728824197ff2
znlccy <756311868@qq.com>
2018-12-04 14:05:35 +0800

    修改产品微服务

--- product-client/src/main/java/com/znlccy/productclient/ProductClientApplication.java
+++ product-client/src/main/java/com/znlccy/productclient/ProductClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductClientApplication.class, args);
+    }
 }
